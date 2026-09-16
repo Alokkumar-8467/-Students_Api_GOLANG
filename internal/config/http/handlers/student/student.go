000bd1ea85834d7e1fcb36881a6ec086b19216af
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-16 17:23:14 +0530

    Update student.go

--- internal/config/http/handlers/student/student.go
+++ internal/config/http/handlers/student/student.go
@@ -16,11 +16,22 @@ import (
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		var student types.Student
-		
+
 		err := json.NewDecoder(r.Body).Decode(&student)
 		if errors.Is(err, io.EOF) {
 			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("Empty Body")))
+			return
+		}
+
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+
+		// For Production application we need to use request validation that what i need form client, that i get or not.
+		if err := validator.New().Struct(student); err != nil {
+			validateErrs := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrs))
 			return
 		}
 
