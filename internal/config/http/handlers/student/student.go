664b3d43c20abddc16513266883b3ee2a0e843ea
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-09-16 17:23:45 +0530

    Update student.go

--- internal/config/http/handlers/student/student.go
+++ internal/config/http/handlers/student/student.go
@@ -16,6 +16,7 @@ import (
 func New() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
+		var student types.Student
 
 		err := json.NewDecoder(r.Body).Decode(&student)
 		if errors.Is(err, io.EOF) {
